a5420a2236ebe812ce4c24919d44213542346526
DavidSoh[catalina] <mail@stdj.me>
2022-04-08 15:44:27 +0800

    add example output files

---
+++ example-v3-shell.sh
@@ -0,0 +1,2 @@
+echo the following commands into example-v3-shell.sh
+echo something into example-v3-shell.sh but not example-v3-log.txt >> example-v3-log.txt
---
+++ log/example-v2-script.sh
@@ -0,0 +1,2 @@
+echo the following commands into shell script
+echo something into log/example-v2-stdout.txt >> log/example-v2-stdout.txt
