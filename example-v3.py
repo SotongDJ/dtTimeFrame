63f6041369cd375658dfce36ff0c85fdfbaa4606
DavidSoh[catalina] <mail@stdj.me>
2022-04-08 15:40:20 +0800

    update example-v3

--- example-v3.py
+++ example-v3.py
@@ -1,41 +1,34 @@
 #!/usr/bin/env python3
 import libTimeTag, argparse, datetime
 parser = argparse.ArgumentParser(description="run some tool")
-parser.add_argument("-s", "--script", help="give command sh script (optional)",type=str)
-parser.add_argument("-n", "--name", help="give name string(optional)",type=str)
-parser.add_argument("-o", "--output", help="give output name (optional)",type=str)
+parser.add_argument("-n", "--name", help="give name string(optional)",type=str,default="something")
+parser.add_argument("-o", "--output", help="give output log name (optional)",type=str,default="example-v3-log.txt")
+parser.add_argument("-e", "--error", help="give error log name (optional)",type=str,default="example-v3-error.txt")
+parser.add_argument("-s", "--script", help="give shell script name(optional)",type=str,default="example-v3.sh")
 args = parser.parse_args()
-if args.name:
-    name = args.name
-else:
-    name = "something"
-if args.output:
-    output = args.output
-else:
-    output = "example-stdout.txt"
 Tool = libTimeTag.tag()
-Tool.log.name = "example-log.txt"
-Tool.error.name = "example-error.txt"
+Tool.log.name = args.output
+Tool.error.name = args.error
 
 Tool.start()
 
-Tool.timeStamp("PREVIEW: {} not EXIST".format(name))
+Tool.timeStamp("PREVIEW: show {}".format(args.name))
 
-Tool.runCommand(F"echo {name}")
+Tool.runCommand(F"echo {args.name}")
 
-Tool.runCommand(F"echo {name} into {output}",export_file=output)
+Tool.runCommand(F"echo {args.name} into {args.output}",export_file=args.output)
 
-Tool.script.name = "example-shell.sh"
-phrase_str = F"echo the following commands into {Tool.script.name}"
+Tool.script.name = args.script
+phrase_str = F"echo the following commands into {args.script}"
 Tool.runCommand(phrase_str)
 
-phrase_str = F"echo {name} into {Tool.script.name} but not {output}"
-Tool.runCommand(phrase_str,export_file=output)
+phrase_str = F"echo {args.name} into {args.script} but not {args.output}"
+Tool.runCommand(phrase_str,export_file=args.output)
 
 Tool.script.name = ""
-Tool.runCommand("echo Turn off shell script export",export_file=output)
+Tool.runCommand("echo Turn off shell script export",export_file=args.output)
 
-phrase_str = F"echo {name} into {output} but not example.sh"
-Tool.runCommand(phrase_str,export_file=output)
+phrase_str = F"echo {args.name} into {args.output} but not example.sh"
+Tool.runCommand(phrase_str,export_file=args.output)
 
 Tool.stop()
