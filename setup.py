2eb3c9c522b6a0864dd1d0b4818c443ddb4f08f9
DavidSoh[ngsubu] <ln@trth.nl>
2023-03-30 12:19:26 +0800

    notes: update usage

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ import setuptools
 
 setuptools.setup(
     name = "dttimeframe",
-    version = "3.6.0",
+    version = "3.6.1",
     author = "David Soh",
     author_email = "ln@trth.nl",
     description = "dtTimeFrame is a module that pack time track functions and subprocess call functions into one single class.",
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
-    url = "https://github.com/unknown/djtimeframe",
+    url = "https://github.com/SotongDJ/dtTimeFrame",
     packages = ["dttimeframe"],
     keywords = [],
     classifiers = [
